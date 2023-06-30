26ad7ced3cdbf011356d8fac6ea15651075a934b
vipulchandan <plenteousvipul90009@gmail.com>
2023-06-30 08:11:34 +0530

    updating database code

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOlYo0egl9UO6SYWnj2EJDE2oZnWvQ310",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "connectify-79301.firebaseapp.com",
   projectId: "connectify-79301",
   storageBucket: "connectify-79301.appspot.com",
   messagingSenderId: "441814169066",
-  appId: "1:441814169066:web:0b1c5190b6d998b2abae93",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-0FH5MQ0CVX"
 };
 
